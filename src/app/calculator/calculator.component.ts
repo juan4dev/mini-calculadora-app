49d7684d516dc1adbc3f2d45065d00566711c8a7
juan <jd.lopez@protonmail.com>
2021-07-01 20:57:09 +0200

    feat: :sparkles: add operations

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,15 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent implements OnInit {
+  public presentValue: number = 0;
+  public storedValue: number = 0;
 
-  public actualValue: number = 0;
+  public operator: string | undefined;
+  public screenValue: number | string = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
-  public getNumber(numberKeyBoard: number) {
-    console.log(typeof numberKeyBoard)
-    this.actualValue += numberKeyBoard;
+  public getNumber(keyboardNumber: number) {
+    this.presentValue = this.screenValue = keyboardNumber;
+    if (this.operator) {
+      this.carryOutOperation();
+    }
+  }
+
+  public resetValue() {
+    this.screenValue = 0;
+    this.presentValue = 0;
+    this.storedValue = 0;
+  }
+
+  public executeOperation() {
+    this.screenValue = this.storedValue;
+    console.log(this.screenValue);
+  }
+
+  public getOperator(operator: string) {
+    this.operator = operator;
+    this.resetScreenAndSaveValue();
+  }
+
+  private resetScreenAndSaveValue() {
+    this.storedValue = Number(this.screenValue);
+    this.screenValue = '';
+  }
+
+  private carryOutOperation() {
+    this.storedValue = eval(
+      `${this.storedValue}${this.operator}${this.presentValue}`
+    );
+    this.operator = undefined;
   }
 }
